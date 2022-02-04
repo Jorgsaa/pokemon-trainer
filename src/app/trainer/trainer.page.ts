88d4a16fba19cf4eac3d38496cef99c70fa9173a
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-04 14:29:27 +0100

    CHORE: generated trainer page with Angular CLI

---
+++ src/app/trainer/trainer.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer',
+  templateUrl: './trainer.page.html',
+  styleUrls: ['./trainer.page.css']
+})
+export class TrainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
