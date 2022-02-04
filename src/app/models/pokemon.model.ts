8444d7fdd014460cf49e56605fa21009863f34e8
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-04 16:19:23 +0100

    CHORE: added fields to `Pokemon` interface

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -1,3 +1,14 @@
-export interface Pokemon {
+import { Ability } from "./ability.model";
+import { Stat } from "./stat.model";
+import { Type } from "./type.model";
 
+export interface Pokemon {
+    name: string
+    id: number
+    sprites: {
+        front_default: string
+    }
+    abilities: Array<{ ability: Ability, is_hidden: boolean, slot: number }>
+    stats: Array<{ base_stat: number, effort: number, stat: Stat }>
+    types: Array<{ slot: number, type: Type }>
 }
