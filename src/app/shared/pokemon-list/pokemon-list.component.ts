3ae84d3aa07bad04da3641a71357f710781034e3
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-06 01:26:36 +0100

    FEAT: `PokemonListComponent` now fetches pokemon using `PokemonService`

--- src/app/shared/pokemon-list/pokemon-list.component.ts
+++ src/app/shared/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pokemon } from 'src/app/models';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonListComponent implements OnInit {
 
-  constructor() { }
+  pokemon$: Observable<Pokemon[]> = of([])
+
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
+    this.pokemon$ = this.pokemonService.fetch()
   }
 
 }
