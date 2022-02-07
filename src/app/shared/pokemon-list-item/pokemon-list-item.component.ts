a86c6c9275a81fd63d96ec878849fcfca5b2c905
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-07 22:53:13 +0100

    CHORE: remove unused import

--- src/app/shared/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/shared/pokemon-list-item/pokemon-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Pokemon, PokemonDetails } from 'src/app/models';
 import { PokemonService } from 'src/app/services/pokemon.service';
