b760060c2965e385eea0e3f52c233dcebc090443
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-07 22:52:51 +0100

    FEAT: added details modal

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pokemon, PokemonDetails } from '../models';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  @Input() pokemon: Pokemon = { name: 'mewtwo', url: 'https://pokeapi.co/api/v2/pokemon/mewtwo' };
+
+  public getTotal = (pokemon?: PokemonDetails) => pokemon?.stats.reduce((prev, stat) => prev + stat.base_stat, 0)
+  
+  pokemonDetails?: PokemonDetails
+
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
+    this.pokemonService.fetchDetails(this.pokemon.name).subscribe({
+      next: (details) => this.pokemonDetails = details
+    })
   }
+
+  
 
 }
