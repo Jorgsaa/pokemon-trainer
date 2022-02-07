1a18766c6bbc26fec837a8093490f984fb2d46d4
JonasBM <jonas.mela@gmail.com>
2022-02-07 15:13:30 +0100

    FEAT: working pokemon-list

--- src/app/trainer/trainer.page.ts
+++ src/app/trainer/trainer.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../models';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { Pokemon, User } from '../models';
+import { PokemonService } from '../services/pokemon.service';
 import { UserLoggedInService } from '../services/user-logged-in.service';
 import { UserService } from '../services/users.service';
 
@@ -12,13 +13,18 @@ export class TrainerPage implements OnInit {
 
   constructor(
     private readonly userSecvice: UserService,
-    private readonly userLoggedInService: UserLoggedInService
+    private readonly userLoggedInService: UserLoggedInService,
+    private pokemonService: PokemonService
   ) { }
 
   ngOnInit(): void {
     // Temporary for testing. User will be sett when the login function works
     this.userSecvice.fetchContacts();
     this.userLoggedInService.setUser(this.userSecvice.users()[0])
+
+    // For pokemon list
+    this.offset = 0
+    this.loadMore()
   }
 
   get user(): User | undefined {
@@ -27,5 +33,47 @@ export class TrainerPage implements OnInit {
 
   get pokemons(): string[] | undefined {
     return this.userLoggedInService.pokemons()
+  }
+
+  //HHHJDKLHJHKLHDJKLHJKLHJKLHJ POKEMON-LIST
+  @Output() cataloguePokemon: Pokemon[] = []
+  
+  pageLength: number = 50
+  offset: number = 0
+  
+
+
+  loadMore1(): void {
+    this.pokemonService.fetch(this.pageLength, this.offset)
+    .subscribe(data => this.cataloguePokemon.push(...data))
+    this.offset += this.pageLength
+    console.log(this.offset);
+  }
+
+  loadMore(): void {
+    const pokemons = this.userLoggedInService.pokemons()
+    if (pokemons) {
+      // for every pokemon the user owns
+      for (const pokemonName of pokemons) {
+        const newPokemon: Pokemon = {
+          name: pokemonName,
+          url: "",
+        }
+        // insert pokemon-object into the list of pokemons that will be displayed
+        this.cataloguePokemon.push(newPokemon)
+        // stop at the specified pageLength
+      }
+    }
+    
+      
+      
+      
+
+    // Next time this function is triggered, load the next specified number of pokemons
+    
+    /* this.pokemonService.fetch(this.pageLength, this.offset)
+    .subscribe(data => this.cataloguePokemon.push(...data))
+    this.offset += this.pageLength
+    console.log(this.offset); */
   }
 }
