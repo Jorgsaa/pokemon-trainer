6a03cbd23892c13fe709f97a34e406e54b769160
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-08 22:52:18 +0100

    CHORE: formatting

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient, } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from "@angular/router";
-import { User } from "../models";
+import { Router } from '@angular/router';
+import { User } from '../models';
 
-const apiUrl = "https://jorgsaa-noroff-assignment-api.herokuapp.com/trainers/";
-const apiKey = "uEzYwhrkm0OmaPQRfHSqz2OsKL8nsxK3AiqVkJkPjCv2lbiLnDyDkzOCGMm1A1gG";
-const userStorageKey = "user";
+const apiUrl = 'https://jorgsaa-noroff-assignment-api.herokuapp.com/trainers/';
+const apiKey =
+  'uEzYwhrkm0OmaPQRfHSqz2OsKL8nsxK3AiqVkJkPjCv2lbiLnDyDkzOCGMm1A1gG';
+const userStorageKey = 'user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private _users: User[] = [];
@@ -18,36 +19,39 @@ export class UserService {
     private readonly http: HttpClient,
     private readonly router: Router
   ) {
-    if(!this._user) {
+    if (!this._user) {
       let localStorageUser = localStorage.getItem(userStorageKey);
 
-      if(!!localStorageUser) {
+      if (!!localStorageUser) {
         this._user = JSON.parse(localStorageUser!);
-      } 
-    }    
+      }
+    }
   }
 
   /*
     Registers new user with username.
     Private because method doesent check if user already exists in api.
   */
-  private newUser(username: string) {      
-    this.http.post<User[]>(apiUrl, 
-      {
-        "username": username,
-        "pokemon": []
-      },
-      {
-        headers: {
-          'X-API-Key': apiKey,
-          'Content-Type': 'application/json'
+  private newUser(username: string) {
+    this.http
+      .post<User[]>(
+        apiUrl,
+        {
+          username: username,
+          pokemon: [],
+        },
+        {
+          headers: {
+            'X-API-Key': apiKey,
+            'Content-Type': 'application/json',
+          },
         }
-      })
-    .subscribe(response => {
-      this._user = response[0];      
-      localStorage.setItem(userStorageKey, JSON.stringify(response))
-      this.router.navigate(["/catalogue"]);
-    });   
+      )
+      .subscribe((response) => {
+        this._user = response[0];
+        localStorage.setItem(userStorageKey, JSON.stringify(response));
+        this.router.navigate(['/catalogue']);
+      });
   }
 
   /*
@@ -56,14 +60,15 @@ export class UserService {
     Routes to /catalogue page after login.
   */
   public login(username: string) {
-    this.http.get<User[]>(apiUrl + `?username=${username}`)
-      .subscribe(response => {
-        if(response.length === 0) {
+    this.http
+      .get<User[]>(apiUrl + `?username=${username}`)
+      .subscribe((response) => {
+        if (response.length === 0) {
           this.newUser(username);
         } else {
           this._user = response[0];
-          localStorage.setItem(userStorageKey, JSON.stringify(response))
-          this.router.navigate(["/catalogue"]);
+          localStorage.setItem(userStorageKey, JSON.stringify(response));
+          this.router.navigate(['/catalogue']);
         }
       });
   }
@@ -72,70 +77,77 @@ export class UserService {
     Catch pokemon
     Add pokemon to the user pokemon list in local storage and api
   */
-public catchPokemon(pokemonName: string): void {  
-  if (this._user) {
-    this._user?.pokemon.push(pokemonName)
-   
-    this.http.patch<User>(apiUrl + this._user?.id, 
-      {
-        "pokemon": this._user?.pokemon
-      },
-      {
-        headers: {
-          'X-API-Key': apiKey,
-          'Content-Type': 'application/json'
-        }
-      })
-    .subscribe({
-      next: (user) => this._user = user,
-      error: (error) => console.log("Error catching pokemon: ", error),
-      complete: () => localStorage.setItem(userStorageKey, JSON.stringify(this._user))
-    })
-  }
- }
+  public catchPokemon(pokemonName: string): void {
+    if (this._user) {
+      this._user?.pokemon.push(pokemonName);
 
-   /*
+      this.http
+        .patch<User>(
+          apiUrl + this._user?.id,
+          {
+            pokemon: this._user?.pokemon,
+          },
+          {
+            headers: {
+              'X-API-Key': apiKey,
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+        .subscribe({
+          next: (user) => (this._user = user),
+          error: (error) => console.log('Error catching pokemon: ', error),
+          complete: () =>
+            localStorage.setItem(userStorageKey, JSON.stringify(this._user)),
+        });
+    }
+  }
+
+  /*
     Free pokemon
     Remove pokemon from the user pokemon list in local storage and api
   */
- public freePokemon(pokemonName: string): void {
-   if (this._user) {
-     const pokemonIndex = this._user?.pokemon.findIndex(userPokemon => userPokemon === pokemonName)
+  public freePokemon(pokemonName: string): void {
+    if (this._user) {
+      const pokemonIndex = this._user?.pokemon.findIndex(
+        (userPokemon) => userPokemon === pokemonName
+      );
 
-     if (pokemonIndex < 0)
-      return
+      if (pokemonIndex < 0) return;
 
-      
       const newList = this._user?.pokemon;
-      newList.splice(pokemonIndex, 1)
+      newList.splice(pokemonIndex, 1);
 
+      this.http
+        .patch<User>(
+          apiUrl + this._user?.id,
+          {
+            pokemon: newList,
+          },
+          {
+            headers: {
+              'X-API-Key': apiKey,
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+        .subscribe({
+          next: (user) => (this._user = user),
+          error: (error) => console.log('Error catching pokemon: ', error),
+          complete: () =>
+            localStorage.setItem(userStorageKey, JSON.stringify(this._user)),
+        });
+    }
+  }
 
-     this.http.patch<User>(apiUrl + this._user?.id, 
-      {
-        "pokemon": newList
-      },
-      {
-        headers: {
-          'X-API-Key': apiKey,
-          'Content-Type': 'application/json'
-        }
-      })
-    .subscribe({
-      next: (user) => this._user = user,
-      error: (error) => console.log("Error catching pokemon: ", error),
-      complete: () => localStorage.setItem(userStorageKey, JSON.stringify(this._user))
-    })
-   }
- }
-
- /*
+  /*
     Logout and clear the user from localStorage.
     Route to root (landing page)
  */
   public logout() {
     this._user = undefined;
     localStorage.removeItem(userStorageKey);
-    this.router.navigate([""]);
+    this.router.navigate(['']);
   }
 
   /*
@@ -160,20 +172,14 @@ public catchPokemon(pokemonName: string): void {
   }
 
   public fetchContacts() {
-    this.http.get<User[]>('https://jorgsaa-noroff-assignment-api.herokuapp.com/trainers')
-    .subscribe({
-      next: (users) => this._users = users,
-      error: (error) => console.log("Error fetching contacts: ", error),
-      complete: () => console.info('complete (fetched trainers)')
-    })
-  }
-
-  public users(): User[] {
-    return this._users;
-  }
-  /*
     this.http.get<User[]>(apiUrl).subscribe({
       next: (users) => (this._users = users),
       error: (error) => console.log('Error fetching contacts: ', error),
       complete: () => console.info('complete (fetched trainers)'),
     });
+  }
+
+  public users(): User[] {
+    return this._users;
+  }
+}
