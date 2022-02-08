9ca6d2d3f93213bbaa1885867696b757985fefdc
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-08 13:46:34 +0100

    CHORE: remove unused service

--- src/app/shared/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/shared/pokemon-list-item/pokemon-list-item.component.ts
@@ -4,7 +4,6 @@ import { Pokemon, PokemonDetails } from 'src/app/models';
 import { PokemonService } from 'src/app/services/pokemon.service';
 import { EventEmitter } from '@angular/core';
 import { UserService } from 'src/app/services/users.service';
-import { UserLoggedInService } from 'src/app/services/user-logged-in.service';
 
 @Component({
   selector: 'app-pokemon-list-item',
@@ -22,9 +21,7 @@ export class PokemonListItemComponent implements OnInit {
 
   constructor(
     private readonly pokemonService: PokemonService,
-    private readonly userService: UserService,
-    private readonly userLoggedInService: UserLoggedInService
-  ) {}
+    private readonly userService: UserService) {}
 
   ngOnInit(): void {
     this.pokemonDetails$ = this.pokemonService.fetchDetails(this.pokemon.name);
